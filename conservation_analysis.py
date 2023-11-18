89d53f2c83fcf5c19a8dd24bb67557a1a7b35e81
B236004-2023 <s2316281@ed.ac.uk>
2023-11-18 15:11:34 +0000

    test_script

--- conservation_analysis.py
+++ conservation_analysis.py
@@ -4,7 +4,7 @@ import os
 parser = argparse.ArgumentParser(
                     prog='Conservation finder',
                     description='The software is designed to do conservation analysis within a group of user-defined species.',
-                    epilog='Version:0.0.1 Author:dongyao')
+                    epilog='Author:B236004-2023')
 
 parser.add_argument('-p', '--proteinname', type=str, help="You need give description here")
 parser.add_argument('-sl', '--species_list', type=str, help="You need give description here")
@@ -12,6 +12,7 @@ parser.add_argument('-on', '--organism_name', type=str, help="You need give desc
 parser.add_argument('-n', '--prefix', type=str)
 parser.add_argument('-w', '--windowsize', type=int)
 parser.add_argument('-f', '--graph_format', type=str)
+parser.add_argument('-m', '--minlen',type=int)
 args = parser.parse_args()
 
 
@@ -20,28 +21,30 @@ args = parser.parse_args()
 
 
 
-def conservation_analysis(fasta_file, out_file_prefix, graph_format, windowsize):
+def conservation_analysis(fasta_file, out_file_prefix, graph_format, windowsize, minlen):
 	multi_sequence_aln = f"emma -sequence fasta_file -outseq {out_file_prefix}.aln -dendoutfile {out_file_prefix}.dnd"
-	align_info = "infoalign {out_file_prefix}.aln {out_file_prefix}.info"
-	#We filter the alignment base on the align info column 7. similarity percent. you can open this to user in further time.
+	align_info = f"infoalign {out_file_prefix}.aln {out_file_prefix}.info"
+	cdm_print = f"cat {out_file_prefix}.info"
+        #We filter the alignment base on the align info column 7. similarity percent. you can open this to user in further time.
 	## current cut off is 0.5
-	filter_the_alignment = "grep -v "#" {out_file_prefix}.info | awk '{print $2, $7, $7/$3}' | awk '$3 >= 0.5 {print $1}'> species_candidate.txt"
+	filter_the_alignment = f"grep -v "#" {out_file_prefix}.info | awk '{print $2, $7, $7/$3}' | awk '$3 >= 0.5 {print $1}'> species_candidate.txt"
 	## we need to extract our alignment by useing pull seq
-	alignment_extraction = "/localdisk/data/BPSM/ICA2/pullseq -i {out_file_prefix}.aln -n species_candidate.txt >{out_file_prefix}.filter.aln"
+	alignment_extraction = f"/localdisk/data/BPSM/ICA2/pullseq -i {out_file_prefix}.aln -n species_candidate.txt >{out_file_prefix}.filter.aln"
 	
 	## conservation analysis
 	plotcon = f"plotcon -sequences {out_file_prefix}.filter.aln -graph {graph_format} -winsize {windowsize}"
-os.system(cmd5)
 
 	motif_search= f"patmatmotifs -sequence {fasta_file} -outfile {out_file_prefix}.motifs"
-os.system(cmd6)
 	
+        antigenic = f"antigenic -sequence {fasta_file} -minlen {minlen} -outfile {out_file_prefix}.antigenic"
+
+        os.system()
 
 
 
 
 if args.species_list == "":
-	Organism_Information = subprocess.check_output("esearch -db taxonomy -query {args.organism_name}  | esummary | xtract -pattern DocumentSummary -element Rank Id", shell=True) 
+	Organism_Information = subprocess.check_output(f"esearch -db taxonomy -query {args.organism_name}  | esummary | xtract -pattern DocumentSummary -element Rank Id", shell=True) 
 	Toxn_level = Organism_Information.split()[0]
 	txid = Organism_Information.split()[1]
 
@@ -62,7 +65,8 @@ if args.species_list == "":
 			conservation_analysis(fasta_file = query_protein_fasta, 
 					out_file_prefix = args.prefix,
 					graph_format = graph_format, 
-					windowsize = args.windowsize)		
+					windowsize = args.windowsize
+                                        minlen = args.minlen)		
 	else:
 		# we didn't offer conservaton analysis within single species
 		print("We can't offer conservation analysis in single species")
@@ -79,7 +83,8 @@ else:
 	conservation_analysis(fasta_file = fasta_file_out,
                                         out_file_prefix = args.prefix,
                                         graph_format = graph_format,
-                                        windowsize = args.windowsize)
+                                        windowsize = args.windowsize
+                                        minlen = args.minlen)
 	
 
 
@@ -94,20 +99,3 @@ else:
 
 
 
-
-
-## Real analysis start from now on
-## query the CDS sequence from nucleotide database
-cmd1 = f'esearch -db nucleotide -query "{args.proteinname}[protein] AND {args.organism_name}[organism]" | efetch -format fasta_cds_na >{args.filename}'
-print(cmd1)
-os.system(cmd1)
-
-cmd5 = f"plotcon -sequences {args.filename}.alignment -graph {args.graph_format} -winsize {args.windowsize}"
-os.system(cmd5)
-
-cmd6 = f"patmatmotifs -sequence {args.filename} -outfile {args.filename}.motifs"
-os.system(cmd6)
-
-print(args.proteinname)
-print(args.species_list)
-print(args.windowsize)
